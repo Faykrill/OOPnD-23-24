4c05db2b05d5857c84fad171807368ffc07efde0
Максим <faykrill@mail.ru>
2024-03-06 10:30:18 +0100

    ini

--- SpaceBattle.Lib/BridgeCommand.cs
+++ SpaceBattle.Lib/BridgeCommand.cs
@@ -2,7 +2,7 @@ namespace SpaceBattle.Lib;
 
 public class BridgeCommand: IBridgeCommand, ICommand
 {
-    private ICommand _internalCommand;
+    public ICommand _internalCommand;
     public BridgeCommand(ICommand internalCommand)
     {
         _internalCommand = internalCommand;
